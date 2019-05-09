fc237b81480039165e13e2ff0cbcfcb47cb30854
Levente Wolf <wlevi2002@gmail.com>
2019-05-09 20:50:55 +0200

    in move method switched to switch expression

--- src/characters/Player.java
+++ src/characters/Player.java
@@ -10,7 +10,7 @@ import handlers.gameHandler.GameMaster;
 
 import java.util.ArrayList;
 
-public class Player extends Character{
+public class Player extends Character {
     public int maxHealth = 20;
     public Weapon currentWeapon;
     private int weaponDamage;
@@ -18,14 +18,14 @@ public class Player extends Character{
     public int damage;
     public ArrayList<Item> inventory = new ArrayList<>();
 
-    public void setDamage(){
+    public void setDamage() {
         damage = handDamage + weaponDamage;
     }
 
-    public void usePotion(){
+    public void usePotion() {
         boolean usedPotion = false;
 
-        if (hasPotion(inventory)){
+        if (hasPotion(inventory)) {
             OutputHandler.showYourPotions(inventory);
             String chosenPotionName = InputHandler.getPotionName();
             for (Item item : inventory) {
@@ -46,31 +46,31 @@ public class Player extends Character{
                 OutputHandler.showWrongChoice();
                 usePotion();
             }
-        }else{
+        } else {
             OutputHandler.showZeroPotions();
         }
     }
 
-    private boolean hasPotion(ArrayList<Item> inventory){
+    private boolean hasPotion(ArrayList<Item> inventory) {
         boolean potion = false;
         for (Item item : inventory) {
-            if(item instanceof Potion) potion = true;
+            if (item instanceof Potion) potion = true;
         }
         return potion;
 
     }
 
-    public void changeWeapon(){
+    public void changeWeapon() {
         boolean hasChanged = false;
 
 
-        if (hasWeapon(inventory)){
+        if (hasWeapon(inventory)) {
             OutputHandler.showYourWeapons(inventory);
             String chosenWeapon = InputHandler.getWeaponName();
 
             for (Item item : inventory) {
-                if(item.name.equals(chosenWeapon)){
-                    if (currentWeapon != null){
+                if (item.name.equals(chosenWeapon)) {
+                    if (currentWeapon != null) {
                         GameMaster.addItemToInventory(currentWeapon, inventory);
                     }
                     currentWeapon = (Weapon) item;
@@ -87,29 +87,29 @@ public class Player extends Character{
                 changeWeapon();
             }
 
-        }else{
+        } else {
             OutputHandler.showZeroWeapons();
         }
     }
 
-    private boolean hasWeapon(ArrayList<Item> inventory){
+    private boolean hasWeapon(ArrayList<Item> inventory) {
         boolean hasWeapon = false;
         for (Item item : inventory) {
-            if(item instanceof Weapon) hasWeapon = true;
+            if (item instanceof Weapon) hasWeapon = true;
         }
         return hasWeapon;
     }
 
-    public void openInventory(){
-        if (inventory.size() == 0){
+    public void openInventory() {
+        if (inventory.size() == 0) {
             OutputHandler.showZeroItems();
-        }else{
+        } else {
             OutputHandler.showYourItems();
             OutputHandler.showInventory(inventory);
         }
     }
 
-    public void move(Map map, GameMaster gameMaster){
+    public void move(Map map, GameMaster gameMaster) {
         int oldXCord = x_cord;
         int oldYCord = y_cord;
 
@@ -117,32 +117,38 @@ public class Player extends Character{
         OutputHandler.showDirections();
 
         boolean hasMoved = false;
+        String direction;
+
         while (!hasMoved) {
-            String direction = InputHandler.getDirection();
-            if ("North".equals(direction)) {
-                y_cord += 1;
-                hasMoved = true;
-            } else if ("South".equals(direction)) {
-                y_cord -= 1;
-                hasMoved = true;
-            } else if ("West".equals(direction)) {
-                x_cord += -1;
-                hasMoved = true;
-            } else if ("East".equals(direction)) {
-                x_cord += 1;
-                hasMoved = true;
-            } else {
-                OutputHandler.showWrongDirection();
-                OutputHandler.showDirections();
+            direction = InputHandler.getDirection();
+            switch (direction) {
+                case "North":
+                    y_cord += 1;
+                    hasMoved = true;
+                    break;
+                case "South":
+                    y_cord -= 1;
+                    hasMoved = true;
+                    break;
+                case "West":
+                    x_cord += -1;
+                    hasMoved = true;
+                    break;
+                case "East":
+                    x_cord += 1;
+                    hasMoved = true;
+                    break;
+                default:
+                    OutputHandler.showWrongDirection();
+                    OutputHandler.showDirections();
+                    break;
             }
         }
 
-
-        gameMaster.hitObject(map, oldXCord, oldYCord);
-
+        //TODO fix bug: hit wall -> new direction printed n times
+        gameMaster.collision(map, oldXCord, oldYCord); //HitWall recursive shit calling move()
+                                                        // again -> newLocation printed n times.
         OutputHandler.showNewLocation(x_cord, y_cord);
-
-
     }
 
     @Override
@@ -164,7 +170,7 @@ public class Player extends Character{
     private void attack(Enemy enemy) {
         int attackDamage = damage - enemy.blockDamage;
 
-        if (attackDamage > 0){
+        if (attackDamage > 0) {
             enemy.health -= attackDamage;
             OutputHandler.showPlayerDamageInfo(enemy, attackDamage);
         } else {
